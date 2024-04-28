5e14fb32b7cfc357d307d86d70e16252cc01c6b7
GideonBature <infoaboutgideon@gmail.com>
2024-04-28 05:12:29 +0100

    set up html etc

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const http = require("http");
-const routes = require("./routes");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
-server.listen(3000);
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+app.listen(3000);
